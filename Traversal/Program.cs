cdbaa4ab2af2233dd9d52802e822ea764f4d04ed
Batuhan18 <54512066+Batuhan18@users.noreply.github.com>
2025-04-30 17:25:13 +0300

    ...

--- Traversal/Program.cs
+++ Traversal/Program.cs
@@ -48,4 +48,12 @@ app.UseEndpoints(endpoints =>
     );
 });
 
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+      name: "areas",
+      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
+});
+
 app.Run();
