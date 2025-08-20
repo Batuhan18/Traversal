51f7a8478c557d45e4a0a0d4a1560432688c88cd
Batuhan18 <batuhanavciogl.18@gmail.com>
2025-08-20 14:07:50 +0300

    ...

--- Traversal/Controllers/DestinationController.cs
+++ Traversal/Controllers/DestinationController.cs
@@ -1,12 +1,16 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Traversal.Controllers
 {
+    [AllowAnonymous]
+
     public class DestinationController : Controller
     {
+     
         DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
         public IActionResult Index()
         {
